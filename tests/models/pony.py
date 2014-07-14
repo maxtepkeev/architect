bedda2fe8ed4f7429b2125a8b7e9a7a8995e748f
Max Tepkeev <tepkeev@gmail.com>
2014-07-14 19:56:31 +0400

    dynamic test models

--- tests/models/pony.py
+++ tests/models/pony.py
@@ -1,18 +1,27 @@
+from __future__ import absolute_import
+
+import datetime
+
 from pony.orm import *
 from architect.orms.pony.mixins import PartitionableMixin
 
 db = Database('postgres', user='postgres', password='', host='localhost', database='architect')
 
-
-class Document(PartitionableMixin, db.Entity):
-    name = Required(unicode)
-    created = Required(datetime.datetime)
-    composite_key(name, created)
-
+# Generation of entities for date range partitioning
+for item in ('day', 'week', 'month', 'year'):
     class PartitionableMeta:
         partition_type = 'range'
         partition_subtype = 'date'
-        partition_range = 'day'
+        partition_range = item
         partition_column = 'created'
+
+    name = 'RangeDate{0}'.format(item.capitalize())
+
+    locals()[name] = type(name, (PartitionableMixin, db.Entity), {
+        '_table_': 'test_rangedate{0}'.format(item),
+        'name': Required(unicode),
+        'created': Required(datetime.datetime),
+        'PartitionableMeta': PartitionableMeta
+    })
 
 db.generate_mapping(create_tables=True)
