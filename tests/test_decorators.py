4da761be79b3b15eca109689d8065c6066561229
Max Tepkeev <tepkeev@gmail.com>
2015-04-07 17:24:40 +0300

    switch from django to peewee

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -7,19 +7,19 @@ class BaseDecoratorTestCase(object):
     def setUp(self):
         self.meta = type('Metaclass', (type,), {})
         self.model = type('Model', (self.meta('NewBase', (object,), {}),), {'save': lambda *args, **kwargs: 'save'})
-        self.model = install('partition', orm='django')(self.model)
+        self.model = install('partition', orm='peewee')(self.model)
 
 
 class InstallDecoratorTestCase(BaseDecoratorTestCase, unittest.TestCase):
     def test_merges_several_features(self):
         self.model = uninstall('operation')(self.model)
-        self.model = install('operation', orm='django')(self.model)
+        self.model = install('operation', orm='peewee')(self.model)
         self.assertIn('partition', self.model.architect.__dict__)
         self.assertIn('operation', self.model.architect.__dict__)
 
     def test_gathers_original_method(self):
         self.childmodel = type('ChildModel', (self.model,), {})
-        self.childmodel = install('partition', orm='django')(self.childmodel)
+        self.childmodel = install('partition', orm='peewee')(self.childmodel)
         self.assertEqual(self.model.save.original(self.model()), 'save')
         self.assertEqual(self.childmodel.save.original(self.childmodel()), 'save')
 
@@ -41,11 +41,11 @@ class InstallDecoratorTestCase(BaseDecoratorTestCase, unittest.TestCase):
 
     def test_raises_feature_install_error(self):
         from architect.exceptions import FeatureInstallError
-        self.assertRaises(FeatureInstallError, lambda: install('foo', orm='django')(self.model))
+        self.assertRaises(FeatureInstallError, lambda: install('foo', orm='peewee')(self.model))
 
     def test_raises_method_autodecorate_error(self):
         from architect.exceptions import MethodAutoDecorateError as MADError
-        self.assertRaises(MADError, lambda: install('partition', orm='django')(mock.Mock(spec=['__name__'])))
+        self.assertRaises(MADError, lambda: install('partition', orm='peewee')(mock.Mock(spec=['__name__'])))
 
 
 class UninstallDecoratorTestCase(BaseDecoratorTestCase, unittest.TestCase):
