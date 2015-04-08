cf1889c948c0ae36422d10434d8e1e2b41bf0b13
Max Tepkeev <tepkeev@gmail.com>
2015-04-08 13:06:18 +0300

    switch to abstract orm

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,61 +1,87 @@
 from . import unittest, mock
 
 from architect import install, uninstall
+from architect.orms.bases import BaseFeature
 
 
 class BaseDecoratorTestCase(object):
     def setUp(self):
+        spec = ['FooFeature', 'BarFeature']
+        self.foo_feature = type('FooFeature', (BaseFeature,), {})
+        self.bar_feature = type('BarFeature', (BaseFeature,), {'dependencies': ('foo',)})
+        foo = mock.Mock(features=mock.Mock(spec=spec, FooFeature=self.foo_feature, BarFeature=self.bar_feature))
+        patcher = mock.patch.dict('sys.modules', {
+            'architect.orms.foo': foo,
+            'architect.orms.foo.features': foo.features
+        })
+        patcher.start()
+        self.addCleanup(patcher.stop)
+
         self.meta = type('Metaclass', (type,), {})
         self.model = type('Model', (self.meta('NewBase', (object,), {}),), {'save': lambda *args, **kwargs: 'save'})
-        self.model = install('partition', orm='peewee')(self.model)
 
 
 class InstallDecoratorTestCase(BaseDecoratorTestCase, unittest.TestCase):
     def test_merges_several_features(self):
-        self.model = uninstall('operation')(self.model)
-        self.model = install('operation', orm='peewee')(self.model)
-        self.assertIn('partition', self.model.architect.__dict__)
-        self.assertIn('operation', self.model.architect.__dict__)
+        self.model = install('bar', orm='foo')(self.model)
+        self.model = uninstall('foo')(self.model)
+        self.model = install('foo', orm='foo')(self.model)
+        self.assertIn('foo', self.model.architect.__dict__)
+        self.assertIn('bar', self.model.architect.__dict__)
 
     def test_gathers_original_method(self):
+        self.bar_feature.decorate = ('save',)
+        self.bar_feature._decorate_save = staticmethod(lambda method: lambda *args, **kwargs: None)
+        self.model = install('bar', orm='foo')(self.model)
         self.childmodel = type('ChildModel', (self.model,), {})
-        self.childmodel = install('partition', orm='peewee')(self.childmodel)
+        self.childmodel = install('bar', orm='foo')(self.childmodel)
         self.assertEqual(self.model.save.original(self.model()), 'save')
         self.assertEqual(self.childmodel.save.original(self.childmodel()), 'save')
 
     def test_register_hooks(self):
-        from architect.orms.bases import BaseFeature
-        foo = mock.Mock()
-        modules = {'architect.orms.foo': foo, 'architect.orms.foo.features': foo.features}
-        with mock.patch.dict('sys.modules', modules):
-            from architect.orms.foo import features
-            features.FooFeature = type('Foo', (BaseFeature,), {
-                'register_hooks': staticmethod(lambda model: setattr(model, 'foo', 'foo'))
-            })
-            self.model = install('foo', orm='foo')(self.model)
-            self.assertEqual(self.model.foo, 'foo')
+        self.foo_feature.register_hooks = staticmethod(lambda model: setattr(model, 'foo', 'foo'))
+        self.model = install('foo', orm='foo')(self.model)
+        self.assertEqual(self.model.foo, 'foo')
+
+    def test_dynamically_changes_model_object(self):
+        self.model = install('foo', orm='foo')(self.model)
+        obj1 = self.model().architect.foo.model_obj
+        self.assertEqual(self.model.__dict__['architect'].map[self.model]['features']['foo'].model_obj, obj1)
+        obj2 = self.model().architect.foo.model_obj
+        self.assertEqual(self.model.__dict__['architect'].map[self.model]['features']['foo'].model_obj, obj2)
 
     def test_raises_orm_error(self):
         from architect.exceptions import ORMError
-        self.assertRaises(ORMError, lambda: install('partition', orm='foo')(self.model))
+        self.assertRaises(ORMError, lambda: install('partition', orm='bar')(self.model))
 
     def test_raises_feature_install_error(self):
         from architect.exceptions import FeatureInstallError
-        self.assertRaises(FeatureInstallError, lambda: install('foo', orm='peewee')(self.model))
+        self.assertRaises(FeatureInstallError, lambda: install('foobar', orm='foo')(self.model))
 
     def test_raises_method_autodecorate_error(self):
         from architect.exceptions import MethodAutoDecorateError as MADError
-        self.assertRaises(MADError, lambda: install('partition', orm='peewee')(mock.Mock(spec=['__name__'])))
+        self.foo_feature.decorate = ('save',)
+        self.assertRaises(MADError, lambda: install('foo', orm='foo')(mock.Mock(spec=['__name__'])))
 
 
 class UninstallDecoratorTestCase(BaseDecoratorTestCase, unittest.TestCase):
     def test_successful_uninstall(self):
-        self.assertIn('partition', self.model.architect.__dict__)
-        self.assertIn('operation', self.model.architect.__dict__)
-        uninstall('partition')(self.model)
-        self.assertNotIn('partition', self.model.architect.__dict__)
-        self.assertNotIn('operation', self.model.architect.__dict__)
+        self.model = install('bar', orm='foo')(self.model)
+        self.assertIn('foo', self.model.architect.__dict__)
+        self.assertIn('bar', self.model.architect.__dict__)
+        uninstall('bar')(self.model)
+        self.assertNotIn('foo', self.model.architect.__dict__)
+        self.assertNotIn('bar', self.model.architect.__dict__)
+
+    def test_uninstall_restores_decorated_method(self):
+        self.foo_feature.decorate = ('save',)
+        self.foo_feature._decorate_save = staticmethod(lambda method: lambda *args, **kwargs: None)
+        self.model = install('foo', orm='foo')(self.model)
+        self.assertIsNone(self.model.save(self.model()))
+        self.model = uninstall('foo')(self.model)
+        self.assertEqual(self.model.save(self.model()), 'save')
 
     def test_raises_feature_uninstall_error(self):
         from architect.exceptions import FeatureUninstallError
-        self.assertRaises(FeatureUninstallError, lambda: uninstall('foo')(self.model))
+        self.model = install('foo', orm='foo')(self.model)
+        self.assertRaises(FeatureUninstallError, lambda: uninstall('bar')(self.model))
