9926a94c33b3a7a406cc24018190423efc49d6c6
Max Tepkeev <tepkeev@gmail.com>
2015-11-07 16:46:08 +0300

    run django.setup only for DJANGO_SETTINGS_MODULE

--- architect/orms/django/__init__.py
+++ architect/orms/django/__init__.py
@@ -1,6 +1,9 @@
 def init():
-    try:
-        import django
-        django.setup()
-    except AttributeError:
-        pass
+    import os
+
+    if 'DJANGO_SETTINGS_MODULE' in os.environ:
+        try:
+            import django
+            django.setup()
+        except AttributeError:
+            pass
