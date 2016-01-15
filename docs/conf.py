a5228bcd75b62f1a642932714e767b52d34fd7d0
Max Tepkeev <tepkeev@gmail.com>
2016-01-15 18:59:33 +0300

    update year [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Architect'
-copyright = u'2015, Max Tepkeev'
+copyright = u'2016, Max Tepkeev'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
