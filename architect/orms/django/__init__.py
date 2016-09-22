3ca39a895bd454b5436d2e1582887c84b743d2ed
Guto Maia <guto@guto.net>
2016-09-22 17:12:20 -0300

    improved support for Django 1.6.1

--- architect/orms/django/__init__.py
+++ architect/orms/django/__init__.py
@@ -1,9 +1,13 @@
 def init():
     import os
+    from distutils.version import StrictVersion
 
     if 'DJANGO_SETTINGS_MODULE' in os.environ:
         try:
             import django
-            django.setup()
+            if StrictVersion(django.get_version()) < StrictVersion('1.7.0'):
+                from django.conf import settings
+            else:
+                django.setup()
         except AttributeError:
             pass
